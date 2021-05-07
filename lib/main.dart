7445fd5f2603a05f2f7e017d643cbe18cd0857d1
Ajay <ajay@gowebknot.com>
2021-05-07 15:31:08 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+// For the testing purposes, you should probably use https://pub.dev/packages/uuid
+String randomString() {
+  var random = Random.secure();
+  var values = List<int>.generate(16, (i) => random.nextInt(255));
+  return base64UrlEncode(values);
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hello'),
+      ),
+    );
+  }
+}
